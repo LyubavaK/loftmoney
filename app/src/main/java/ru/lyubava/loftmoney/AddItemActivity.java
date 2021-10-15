6a990c88fe4398639c5b19a8a8ff4e7990b670ec
Любава Кузнецова <lyubava99kuznetsova@gmail.com>
2021-10-16 00:32:06 +0300

    add_item_activity

--- app/src/main/java/ru/lyubava/loftmoney/AddItemActivity.java
+++ app/src/main/java/ru/lyubava/loftmoney/AddItemActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class SecondActivity extends AppCompatActivity {
+public class AddItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_add_item);
 
         int costId = getIntent().getIntExtra("COST_ID", 0);
         Log.e("TAG", "Cost id = " + costId);
--- app/src/main/java/ru/lyubava/loftmoney/MainActivity.java
+++ app/src/main/java/ru/lyubava/loftmoney/MainActivity.java
@@ -16,13 +16,10 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent newActivity = new Intent(getApplicationContext(), SecondActivity.class);
+                Intent newActivity = new Intent(getApplicationContext(), AddItemActivity.class);
                 newActivity.putExtra("COST_ID", 1);
                 startActivity(newActivity);
             }
         });
-
-        // TO-DO: Here we will work with recyclerview
-
     }
 }
